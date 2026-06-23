05605a447a167ad85fc12340c23b96472cc35e1d
Keith Yeung <kungfukeith11@gmail.com>
2026-06-23 21:45:50 +0800

    parser: Allow turbofish syntax in path segments

--- crates/parser/src/parser/path.rs
+++ crates/parser/src/parser/path.rs
@@ -39,12 +39,20 @@ impl super::Parse for PathSegmentScope {
             Some(kind) if is_path_segment(kind) => {
                 parser.bump();
 
-                if parser.current_kind_same_line() == Some(SyntaxKind::Lt)
-                    && !(is_lt_eq(parser) || is_lshift(parser))
+                let is_turbofish = parser.current_kind_same_line() == Some(SyntaxKind::Colon2)
+                    && parser.peek_two() == (Some(SyntaxKind::Colon2), Some(SyntaxKind::Lt));
+
+                if (is_turbofish
+                    || (parser.current_kind_same_line() == Some(SyntaxKind::Lt)
+                        && !(is_lt_eq(parser) || is_lshift(parser))))
                     && parser.dry_run(|parser| {
+                        parser.bump_if(SyntaxKind::Colon2);
                         parser.parses_without_error(GenericArgListScope::new(self.is_expr))
                     })
                 {
+                    if is_turbofish {
+                        parser.bump_expected(SyntaxKind::Colon2);
+                    }
                     parser
                         .parse(GenericArgListScope::new(self.is_expr))
                         .expect("dry_run suggests this will succeed");
